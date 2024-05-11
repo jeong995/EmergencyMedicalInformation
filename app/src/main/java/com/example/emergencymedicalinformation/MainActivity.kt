195c5009b18bc01c13705d2c14530b3e8a4b3db4
Jeong <ibaggim049@gmail.com>
2024-05-11 19:06:41 +0900

    second commit

--- app/src/main/java/com/example/emergencymedicalinformation/MainActivity.kt
+++ app/src/main/java/com/example/emergencymedicalinformation/MainActivity.kt
@@ -1,20 +1,72 @@
 package com.example.emergencymedicalinformation
 
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isVisible
+import com.example.emergencymedicalinformation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.goInputActivityButton.setOnClickListener {
+            val intent = Intent(this, EditActivity::class.java)
+            startActivity(intent)
         }
+
+        binding.deleteButton.setOnClickListener {
+            deleteDate()
+        }
+
+        binding.emergencyContactLayer.setOnClickListener {
+            with(Intent(Intent.ACTION_VIEW)) {
+                val phoneNumber = binding.emergencyContactValueTextView.text.toString()
+                    .replace("-", "")
+                data = Uri.parse("tel${phoneNumber}")
+                startActivity(this)
+            }
+        }
+    }
+
+    override fun onResume() {
+        getDateAndUiUpdate()
+        super.onResume()
+    }
+
+    private fun getDateAndUiUpdate() {
+        with(getSharedPreferences(USER_INFORMATION, Context.MODE_PRIVATE)) {
+            binding.nameValueTextView.text = getString(NAME, "미정")
+            binding.birthdateValueTextView.text = getString(BIRTHDATE, "미정")
+            binding.bloodTypeValueTextView.text = getString(BLOOD_TYPE, "미정")
+            binding.emergencyContactValueTextView.text = getString(EMERGENCY_CONTACT, "미정")
+
+            val warning = getString(WARING, "")
+            binding.warningTextView.isVisible = warning.isNullOrEmpty().not()
+            binding.warningValueTextView.isVisible = warning.isNullOrEmpty().not()
+            if (warning.isNullOrEmpty()) {
+                binding.warningValueTextView.text = warning
+            }
+        }
+    }
+
+    private fun deleteDate() {
+        with(getSharedPreferences(USER_INFORMATION, MODE_PRIVATE).edit()) {
+            clear()
+            apply()
+            getDateAndUiUpdate()
+        }
+
+        Toast.makeText(this, "초기화가 완료되었습니다", Toast.LENGTH_LONG).show()
     }
 }
